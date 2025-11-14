62c2de01d0ad80da4ea7c3c6da31665248b2b1c4
andy1764 <chen1764@gmail.com>
2025-11-14 12:56:17 -0500

    remove gamlss and mqgam, add examples and documentation

--- R/refint.R
+++ R/refint.R
@@ -1,98 +1,178 @@
-#' Title
+#' Constructing Reference Intervals
 #'
-#' @param x
-#' @param ...
+#' `refint` is a generic function used to construct `pct`% reference intervals
+#' from a model object. Currently supports both distributional regression models
+#' (\link[stats]{lm} and \link[gamlss2]{gamlss2}) and quantile regression models
+#' (\link[quantreg]{rq} and \link[qgam]{qgam}).
 #'
-#' @return
+#' @param object a model object used to construct the reference interval
+#' @param upper for quantile regression, the upper quantile fit
+#' @param pct for distributional regression, the desired percentage
+#' @param ... additional arguments passed internally to \link[stats]{predict}
+#'
+#' @returns `refint` returns a list containing:
+#' \item{fit}{original model fit for \link[stats]{lm} and \link[gamlss2]{gamlss2}}
+#' \item{fits}{lower and upper fits for \link[quantreg]{rq} and \link[qgam]{qgam}}
+#' \item{terms}{variables used in the original model fit}
+#' \item{get.ri}{function used to get reference interval from model fit(s)}
+#' \item{pct}{percentage reference interval (i.e. 95 refers to 95% reference interval}
+#'
+#' @import stats
 #' @export
 #'
+#' @seealso
+#' \link[ReForm]{predict.refint} for applying reference intervals to new data
+#' \link[ReForm]{reform} for calibration to a new sample
+#'
+#' @rdname refint
 #' @examples
-#' # example for gamlss2
-#' library(gamlss2)
-#' fit <- gamlss2(Sepal.Length ~ Petal.Width + Species, data = iris[1:110,])
-#' ri <- refint(fit, 95)
-#' predict(ri, newdata = iris[111:150,])
-refint <- function(x, ...) {
+#' # example for lm (not recommended)
+#' ri <- refint(
+#'   lm(Sepal.Length ~ Petal.Width + Species,
+#'     data = iris[-(1:20),])
+#' )
+#' predict(ri, newdata = iris[1:20,])
+#'
+#' # example for quantreg::rq
+#' if (require("quantreg")) {
+#'   ri <- refint(
+#'     rq(Sepal.Length ~ Petal.Length + Species,
+#'       data = iris[-(1:20),], tau = 0.025),
+#'     rq(Sepal.Length ~ Petal.Length + Species,
+#'       data = iris[-(1:20),], tau = 0.975)
+#'   )
+#'   predict(ri, newdata = iris[1:20,])
+#' }
+#'
+#' # example for qgam
+#' if (require("quantreg")) {
+#'   ri <- refint(
+#'     qgam(Sepal.Length ~ s(Petal.Length) + Species,
+#'       data = iris[-(1:20),], qu = 0.025),
+#'     qgam(Sepal.Length ~ s(Petal.Length) + Species,
+#'       data = iris[-(1:20),], qu = 0.975)
+#'   )
+#'   predict(ri, newdata = iris[1:20,])
+#' }
+#'
+#' if (require("gamlss2")) {
+#'   ri <- refint(
+#'     gamlss2(Sepal.Length ~ s(Petal.Length) + Species |
+#'       s(Petal.Length) + Species, data = iris[-(1:20),],
+#'       family = BCT))
+#'   predict(ri, newdata = iris[1:20,])
+#' }
+refint <- function(object, ...) {
   UseMethod("refint")
 }
 
+#' @rdname refint
 #' @export
-refint.gamlss <- function(fit, pct = 95, ...) {
-  # helper function to get refint from model
-  get.ri <- function(alpha, newdata, ...) {
-    params = list(
-      mu = predict(fit, newdata = newdata, what = "mu", type = "response", ...),
-      sigma = predict(fit, newdata = newdata, what = "sigma", type = "response", ...))
-    pred <- list(
-      do.call(get(paste("q", fit$family[1], sep = "")), c(list(alpha/2), params)),
-      do.call(get(paste("q", fit$family[1], sep = "")), c(list(1-alpha/2), params)))
+refint.lm <- function(object, pct = 95, ...) {
+  get.ri <- function(newdata, ...) {
+    alpha <- 1-pct/100
+    params <- predict(object, newdata = newdata, ...)
+    pred <- list(qnorm(alpha/2, mean = params, sd = summary(object)$sigma),
+                 qnorm(1-alpha/2, mean = params, sd = summary(object)$sigma))
     names(pred) <- c("lower", "upper")
     pred
   }
 
-  out <- list(fit = fit, terms = as.character(attr(terms(fit), "variables"))[-1],
-              get.ri = get.ri, pct = pct)
+  out <- list(fit = object, terms = names(object$model), get.ri = get.ri,
+              pct = pct)
   class(out) <- "refint"
   out
 }
 
+#' @rdname refint
 #' @export
-refint.gamlss2 <- function(x, pct = 95, ...) {
-  # remove reference data
-  x$model[] <- NA
-  x$y[] <- NA
-  x$x[] <- NA
-
-  # helper function to get refint from model
-  get.ri <- function(x, alpha, newdata, ...) {
-    params = predict(x, newdata = newdata, ...)
-    pred <- list(x$family$quantile(alpha/2, par = params),
-                x$family$quantile(1-alpha/2, par = params))
+refint.gamlss2 <- function(object, pct = 95, ...) {
+  get.ri <- function(newdata, ...) {
+    alpha <- 1-pct/100
+    params = predict(object, newdata = newdata, ...)
+    pred <- list(object$family$quantile(alpha/2, par = params),
+                 object$family$quantile(1-alpha/2, par = params))
     names(pred) <- c("lower", "upper")
     pred
   }
 
-  out <- list(fit = x, terms = names(x$model), get.ri = get.ri, pct = pct)
+  out <- list(fit = object, terms = names(object$model), get.ri = get.ri,
+              pct = pct)
   class(out) <- "refint"
   out
 }
 
+#' @rdname refint
 #' @export
-refint.mqgam <- function(x, pct = 95, ...) {
-  alpha = 1 - pct/100
-  tryCatch(qdo(x, c(alpha/2, 1-alpha/2)),
-           error = function(e) stop("Quantiles not found in model fit"))
-
-  # helper function to get refint from model
-  get.ri <- function(x, alpha, newdata, ...) {
-    pred <- qgam::qdo(x, c(alpha/2, 1-alpha/2), predict, ...)
-    names(pred) <- c("lower", "upper")
-    pred
+refint.rq <- function(object, upper, ...) {
+  if (!all.equal(object$tau, 1-upper$tau)) {
+    stop("Lower and upper fits not matched")
+  }
+  if (!identical(names(object$model), names(upper$model))) {
+    stop("Lower and upper fits have different terms")
   }
 
-  out <- list(fit = fit, terms = names(fit$model), get.ri = get.ri, pct = pct)
+  get.ri <- function(newdata, ...) {
+    list(lower = predict(object, newdata = newdata, ...),
+         upper = predict(upper, newdata = newdata, ...))
+  }
+  pct <- 100*(1-2*object$tau)
+  out <- list(fits = list(lower = object, upper = upper),
+              terms = names(object$model), get.ri = get.ri, pct = pct)
   class(out) <- "refint"
   out
 }
 
-#' @param x
+#' @rdname refint
+#' @export
+refint.qgam <- function(object, upper, ...) {
+  if (!all.equal(object$family$getQu(), 1-upper$family$getQu())) {
+    stop("Lower and upper fits not matched")
+  }
+  if (!identical(names(object$model), names(upper$model))) {
+    stop("Lower and upper fits have different terms")
+  }
+
+  get.ri <- function(newdata, ...) {
+    list(lower = predict(object, newdata = newdata, ...),
+         upper = predict(upper, newdata = newdata, ...))
+  }
+  pct <- 100*(1-2*object$family$getQu())
+  out <- list(fits = list(lower = object, upper = upper),
+              terms = names(object$model), get.ri = get.ri, pct = pct)
+  class(out) <- "refint"
+  out
+}
+
+#' Predict Method for Reference Intervals
 #'
-#' @param newdata
-#' @param ...
+#' Apply fitted reference interval from \link[ReForm]{refint} to new data
+#'
+#' @param newdata input variables used to get reference intervals
+#' @param ... additional arguments passed to \link[stats]{predict}
+#'
+#' @returns a data frame containing lower and upper bounds of the reference
+#' interval, the test observation, and logicals for whether it is above or below
 #'
 #' @export
-predict.refint <- function(x, newdata, ...) {
-  if (any(!(x$terms %in% names(newdata)))) {stop("Terms missing from newdata")}
+predict.refint <- function(object, newdata, ...) {
+  if (any(!(object$terms %in% names(newdata)))) {stop("Terms missing from newdata")}
 
-  out <- x$get.ri(1 - x$pct/100, newdata)
-  out[[x$terms[1]]] <-  newdata[,x$terms[1]]
-  out$above <- newdata[,x$terms[1]] < out[[1]]
-  out$below <- newdata[,x$terms[1]] > out[[2]]
+  out <- object$get.ri(newdata, ...)
+  out[[object$terms[1]]] <-  newdata[,object$terms[1]]
+  out$above <- newdata[,object$terms[1]] < out[[1]]
+  out$below <- newdata[,object$terms[1]] > out[[2]]
 
   data.frame(out)
 }
 
+#' @export
 print.refint <- function(x) {
-  cat(x$pct, "% reference interval using model of class ", class(x$fit)[1],
-      sep = "")
+  if (is.null(x$fits)) {
+    cat(x$pct, "% reference interval using model of class ", class(x$fit)[1],
+        sep = "")
+  } else {
+    cat(x$pct, "% reference interval using models of class ",
+        class(x$fits[[1]])[1], sep = "")
+  }
 }
--- R/reform.R
+++ R/reform.R
@@ -1,10 +1,14 @@
 #' Reference interval calibration via conFormal prediction (ReForm)
 #'
+#' `reform` takes a fitted reference interval and calibrates to a new sample
+#' using split conformalized quantile regression (Romano, et al., 2019).
+#' `reform` requires a calibration set from the new sample (`newdata`). For
+#' more details on calibration set size and ReForm properties, refer to our
+#' preprint Chen et al., 2025.
 #'
-#'
-#' @param x `refint` object, typically result of \link[ReForm]{refint}
-#' @param newdata calibration data used to fit ReForm
-#' @param ... other parameters passed internally to `predict`
+#' @param x `refint` object obtained from \link[ReForm]{refint}
+#' @param caldata calibration data used to fit ReForm
+#' @param ... additional arguments passed internally to \link[stats]{predict}
 #'
 #' @return
 #' @export
@@ -17,22 +21,37 @@
 #' interval to new observations
 #'
 #' @examples
-#' # example for gamlss2
-#' library(gamlss2)
-#' fit <- gamlss2(Sepal.Length ~ Petal.Width + Species, data = iris[1:110,])
-#' ri <- refint(fit, 95)
-#' reformint <- reform(ri, newdata = iris[111:130,])
-#' predict(reformint, newdata = iris[131:150,])
-reform <- function(x, newdata, ...) {
-  if (any(!(x$terms %in% names(newdata)))) {stop("Terms missing from newdata")}
-
+#' # example for lm (not recommended)
+#' ri <- refint(
+#'   lm(Sepal.Length ~ Petal.Width + Species,
+#'     data = iris[-(1:40),])
+#' )
+#' reformed_ri <- reform(ri, caldata = iris[1:30,])
+#' predict(reformed_ri, newdata = iris[31:40,])
+#'
+#' # example for quantreg::rq
+#' if (require("quantreg")) {
+#'   ri <- refint(
+#'     rq(Sepal.Length ~ Petal.Length,
+#'       data = iris[1:50,], tau = 0.025),
+#'     rq(Sepal.Length ~ Petal.Length,
+#'       data = iris[1:50,], tau = 0.975)
+#'   )
+#'   reformed_ri <- reform(ri, caldata = iris[1:30,])
+#'   predict(reformed_ri, newdata = iris[31:40,])
+#' }
+#'
+#' @references
+#' Romano, Y., Patterson, E., & Candes, E. (2019). Conformalized quantile regression. Advances in neural information processing systems, 32.
+reform <- function(x, caldata, ...) {
+  if (any(!(x$terms %in% names(caldata)))) {stop("Terms missing from caldata")}
   alpha = 1 - x$pct/100
-  nc <- nrow(newdata)
+  nc <- nrow(caldata)
 
-  # calibrate using ReForm
-  pred <- x$get.ri(x$fit, alpha, newdata)
-  res <- data.frame(lower = pred[[1]] - newdata[,x$terms[1]],
-                    upper = newdata[,x$terms[1]] - pred[[2]])
+  # calibrate using CQR
+  pred <- x$get.ri(caldata)
+  res <- data.frame(lower = pred[[1]] - caldata[,x$terms[1]],
+                    upper = caldata[,x$terms[1]] - pred[[2]])
   q_cut <- min(ceiling((1 - alpha/2)*(nc+1)), nc)
   q <- apply(res, 2, function(x) sort(x)[q_cut])
 
@@ -40,7 +59,7 @@ reform <- function(x, newdata, ...) {
   out$nc <- nc
   out$res <- res
   out$cali <- q
-  out$cali.df <- newdata
+  out$cali.df <- caldata
   class(out) <- "reformint"
   out
 }
@@ -55,7 +74,7 @@ predict.reformint <- function(x, newdata, ...) {
   if (any(!(x$terms %in% names(newdata)))) {stop("Terms missing from newdata")}
 
   # apply ReForm calibration
-  out <- x$get.ri(x$fit, 1 - x$pct/100, newdata)
+  out <- x$get.ri(newdata, ...)
   out$lower <- out$lower - x$cali[1]
   out$upper <- out$upper + x$cali[2]
 
@@ -80,7 +99,13 @@ plot.reformint <- function(x, var, ...) {
   par(op)
 }
 
+#' @export
 print.reformint <- function(x) {
-  cat(x$pct, "% ReFormed reference interval using model of class ", class(x$fit),
-      ", calibrated on ", x$nc, " observations", sep = "")
+  if (is.null(x$fits)) {
+    cat(x$pct, "% ReFormed reference interval using model of class ", class(x$fit),
+        ", calibrated on ", x$nc, " observations", sep = "")
+  } else {
+    cat(x$pct, "% ReFormed reference interval using models of class ", class(x$fits[[1]]),
+        ", calibrated on ", x$nc, " observations", sep = "")
+  }
 }
